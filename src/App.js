9dcd02a18b5264915fca1faf74bce5ad89af787c
ClementPremartin <clementpremartin@gmail.com>
2022-04-11 17:59:14 +0200

    finish the quest

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { useState } from 'react';
+import QuoteCard from './Components/QuoteCard';
+import axios from 'axios'
+
+const initialQuote= {
+  "quote": "By chilling my loins I increase the chances of impregnating my wife.",
+  "character": "Apu Nahasapeemapetilon",
+  "image": "https://cdn.glitch.com/3c3ffadc-3406-4440-bb95-d40ec8fcde72%2FApuNahasapeemapetilon.png?1497567511629",
+  "characterDirection": "Left"
+  }
 
 function App() {
+  
+  const getQuote = () => {
+    axios
+    .get("https://simpsons-quotes-api.herokuapp.com/quotes")
+    .then((response) => response.data)
+    .then((data) => {
+      console.log(data);
+      setSimpsonQuote(data[0])
+    })  
+  }
+
+  const [simpsonQuote, setSimpsonQuote]= useState(initialQuote);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <QuoteCard props={simpsonQuote}/>
+     <button type="button" onClick={getQuote}>get Quote</button>
     </div>
   );
 }
